b3d2759098c9b7c285b4890db1771bdfee34bce2
saurabhshandilay4 <43968239+saurabhshandilay4@users.noreply.github.com>
2022-04-22 14:18:30 +0530

    Update app3.py

--- app3.py
+++ app3.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import pandas_datareader as data
-from keras.models import load_model, load_weights
+from keras.models import load_model
 import streamlit as st
 from datetime import date
 import math
